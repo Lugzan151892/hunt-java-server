4d9801143c48395abd302260ddd02026f56b4e67
Denis Gukov <denis.guk@konomic.com>
2024-10-23 16:39:13 +0300

    banned_users: removed extra code

--- src/main/java/lugzan/co/huntjavaserver/services/jwtservice/JwtService.java
+++ src/main/java/lugzan/co/huntjavaserver/services/jwtservice/JwtService.java
@@ -2,7 +2,6 @@ package lugzan.co.huntjavaserver.services.jwtservice;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -15,24 +14,11 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-// import lugzan.co.huntjavaserver.models.user.UserModel;
-import lugzan.co.huntjavaserver.utils.PrivateVariables;
-
 @Service
 public class JwtService {
 
     private static final String SECRET_KEY = "myPythonSecretKeyForDatabaseOnServerLocalmyPythonSecretKeyForDatabaseOnServerLocalmyPythonSecretKeyForDatabaseOnServerLocal";
     private static final int millisecondsInHour = 60000 * 60;
-
-    // @SuppressWarnings("deprecation")
-    // public String generateToken(UserModel user) {
-    //     return Jwts.builder()
-    //             .claim("username", user.getUsername())
-    //             .setIssuedAt(new Date())
-    //             .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 часов
-    //             .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
-    //             .compact();
-    // }
 
     static public SecretKey getSecretKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
@@ -46,7 +32,7 @@ public class JwtService {
 
     static public Claims getTokenData (String token) {
         return Jwts.parser()
-                .verifyWith(PrivateVariables.getSecretKey())
+                .verifyWith(getSecretKey())
                 .build()
                 .parseSignedClaims(token)
                 .getPayload();
@@ -57,7 +43,7 @@ public class JwtService {
                 .claim("user", data)
                 .subject(userName)
                 .expiration(new Date(System.currentTimeMillis() + millisecondsInHour * 10))
-                .signWith(PrivateVariables.getSecretKey())
+                .signWith(getSecretKey())
                 .compact();
     }
 
@@ -66,7 +52,7 @@ public class JwtService {
                 .claim("user", data)
                 .subject(userName)
                 .expiration(new Date(System.currentTimeMillis() + millisecondsInHour * 168))
-                .signWith(PrivateVariables.getSecretKey())
+                .signWith(getSecretKey())
                 .compact();
     }
 }
